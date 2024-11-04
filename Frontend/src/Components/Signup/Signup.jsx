24cff3e20c2d2fd4350185905aa7d922bd5d3485
Aman-Papaney <amanpapaney2003@gmail.com>
2024-11-04 20:59:25 +0530

    signup

--- Frontend/src/Components/Signup/Signup.jsx
+++ Frontend/src/Components/Signup/Signup.jsx
@@ -1,17 +1,66 @@
+import {useEffect, useState} from "react"
+
 import "./signup.css"
-import logo from "../../assets/yt_logo.png"
+import yt_logo from "../../assets/yt_logo.png"
 
 const Signup = () => {
+	const [username, setUsername] = useState("")
+	const [email, setEmail] = useState("")
+	const [phone, setPhone] = useState("")
+	const [password, setPassword] = useState("")
+	const [logo, setLogo] = useState(null)
+	const [imageUrl, setImageUrl] = useState(null)
+
+
+	useEffect(() => {
+		console.log(logo)
+	}, [logo])
+
+  const fileHandler = (e) => {
+    console.log(logo);
+    
+    setLogo(e.target.files[0])
+    setImageUrl(URL.createObjectURL(e.target.files[0]))
+  }
+
 	return (
 		<div className='signup-container'>
 			<div className='form-container'>
 				<div className='logo'>
-					<img src={logo} alt='YouTube' />
+					<img src={yt_logo} alt='YouTube' />
 				</div>
 				<div className='form'>
-					<input type='email' placeholder='Email' />
-					<input type='text' placeholder='Username' />
-					<input type='password' placeholder='Password' />
+					<input
+						onChange={(e) => {
+							setEmail(e.target.value)
+						}}
+						type='email'
+						placeholder='Email'
+					/>
+					<input
+						onChange={(e) => {
+							setUsername(e.target.value)
+						}}
+						type='text'
+						placeholder='Username'
+					/>
+					<input
+						onChange={(e) => {
+							setPhone(e.target.value)
+						}}
+						type='phone'
+						placeholder='Phone Number'
+					/>
+					<input
+						onChange={(e) => {
+							setPassword(e.target.value)
+						}}
+						type='password'
+						placeholder='Password'
+					/>
+					<input onChange={fileHandler} type='file' />
+					<img src={imageUrl} alt=' ' />
+					<button type='submit'>Submit</button>
 				</div>
 			</div>
 		</div>
